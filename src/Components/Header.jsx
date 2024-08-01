843207cf6cb68293c832acec43f49c9834961724
sheikhmuhammadzain <zaingee320@gmail.com>
2024-08-01 22:05:14 +0500

    profile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,9 @@
-
 const Header = () => {
   return (
     <div className="flex items-center justify-center border-b py-5 shadow-md">
-      
       <img src="../../public/group-84152-1@2x.png" height={36} width={166} alt="logo" />
-
     </div>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
