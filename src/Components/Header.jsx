3bd8a1e09df69d23c054972b369def41be7f47f8
sheikhmuhammadzain <zaingee320@gmail.com>
2024-08-01 22:07:38 +0500

    again changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex items-center justify-center border-b py-5 shadow-md">
-      <img src="../../public/group-84152-1@2x.png" height={36} width={166} alt="logo" />
+      <img src="/group-84152-1@2x.png" height={36} width={166} alt="logo" />
     </div>
   );
 };
