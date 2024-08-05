27af54b2f07284ff793478d61a37ea9c6e6d2b28
sheikhmuhammadzain <zaingee320@gmail.com>
2024-08-05 20:26:32 +0500

    more upadted

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -99,15 +99,15 @@ const FinancialOverview = () => {
       </div>
 
       <div className="grid grid-cols-3 gap-4 mb-6">
-        <div className="bg-white p-4 rounded shadow">
-          <h2 className="text-sm text-gray-500">Total Franchise</h2>
+        <div className="bg-white px-4 pb-9 pt-4 rounded shadow">
+          <h2 className="text-sm text-gray-500 pb-2">Total Franchise</h2>
           <p className="text-2xl font-bold">20</p>
         </div>
-        <div className="bg-white p-4 rounded shadow">
+        <div className="bg-white px-4 pb-9 pt-4  rounded shadow">
           <h2 className="text-sm text-gray-500">Total Sales</h2>
           <p className="text-2xl font-bold">$90,000</p>
         </div>
-        <div className="bg-white p-4 rounded shadow">
+        <div className="bg-white px-4 pb-9 pt-4  rounded shadow">
           <h2 className="text-sm text-gray-500">Total Royalty</h2>
           <p className="text-2xl font-bold">$6000</p>
         </div>
