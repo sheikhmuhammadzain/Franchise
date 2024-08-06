bf320033dc8d274a84fc135d93cd9e412c7c449c
sheikhmuhammadzain <zaingee320@gmail.com>
2024-08-06 14:21:07 +0500

    added filter

--- src/Components/ManageUser.jsx
+++ src/Components/ManageUser.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { IoFilterOutline } from "react-icons/io5";
+import { RiDownloadLine } from "react-icons/ri";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { Input } from "@chakra-ui/react";
 import {
   Button,
   Dialog,
@@ -23,11 +34,105 @@ import "../index.css";
 
 function ManageUser() {
   const [openDialog, setOpenDialog] = useState(false);
+  const [id, setId] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [franchiseName, setFranchiseName] = useState("");
+  const [email, setEmail] = useState("");
+  const [plan, setPlan] = useState("All");
+  const [status, setStatus] = useState("All");
+  const [openTheSearch, setopenTheSearch] = useState(false);
+
   const [rows, setRows] = useState([
-    // { id: 1, name: 'Snow', age: 35, full_name: 'Jon Snow', franchise: 'KFC', email: 'jonsnow@gmail.com', last_login: '12-Oct-2024 10:34:09', plan: 'Basic', status: 'Active' },
-    // { id: 2, name: 'Lannister', age: 42, full_name: 'Cersei Lannister', franchise: 'Google', email: 'cerseilan@gmail.com', last_login: '12-Oct-2024 10:34:09', plan: 'Pro', status: 'Active' },
-    // { id: 3, name: 'Stark', age: 26, full_name: 'Arya Stark', franchise: 'Under 500', email: 'aryastark@gmail.com', last_login: '-', plan: 'Basic', status: 'Active' },
-    // { id: 4, name: 'Targaryen', age: 30, full_name: 'Daenerys Targaryen', franchise: 'Fume', email: 'daenerys@gmail.com', last_login: '-', plan: 'Basic', status: 'Active' },
+    {
+      id: 1,
+      full_name: "Hedwig F. Nguyen",
+      franchise: "KFC",
+      email: "ArcuvelFoun@gmail.com",
+      last_login: "-",
+      plan: "Basic",
+      status: "Inactive",
+    },
+    {
+      id: 2,
+      full_name: "Genevieve U. Watts",
+      franchise: "-",
+      email: "ArcuVelFoun@gmail.com",
+      last_login: "-",
+      plan: "Pro",
+      status: "Active",
+    },
+    {
+      id: 3,
+      full_name: "Genevieve U. Watts",
+      franchise: "Google",
+      email: "ArcuVelFoun@gmail.com",
+      last_login: "12-Oct-2024 10:34:09",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 4,
+      full_name: "Genevieve U. Watts",
+      franchise: "-",
+      email: "ArcuVelFoun@gmail.com",
+      last_login: "-",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 5,
+      full_name: "Genevieve U. Watts",
+      franchise: "-",
+      email: "ArcuvelFoun@gmail.com",
+      last_login: "-",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 6,
+      full_name: "Genevieve U. Watts",
+      franchise: "-",
+      email: "ArcuVelFoun@gmail.com",
+      last_login: "-",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 7,
+      full_name: "Genevieve U. Watts",
+      franchise: "Primal",
+      email: "ArcuvelFoun@gmail.com",
+      last_login: "12-Oct-2024 10:34:09",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 8,
+      full_name: "Genevieve U. Watts",
+      franchise: "Crazzy Phils",
+      email: "ArcuvelFoun@gmail.com",
+      last_login: "12-Oct-2024 10:34:09",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 9,
+      full_name: "Genevieve U. Watts",
+      franchise: "Under 500",
+      email: "ArcuVelFoun@gmail.com",
+      last_login: "12-Oct-2024 10:34:09",
+      plan: "Basic",
+      status: "Active",
+    },
+    {
+      id: 10,
+      full_name: "Genevieve U. Watts",
+      franchise: "Fume",
+      email: "ArcuvelFoun@gmail.com",
+      last_login: "12-Oct-2024 10:34:09",
+      plan: "Basic",
+      status: "Active",
+    },
   ]);
 
   const [selectedRows, setSelectedRows] = useState([]);
@@ -125,8 +230,158 @@ function ManageUser() {
             <li className=" text-center">Sales</li>
           </Link>
         </ul>
+
+        {/* seacrhdiv */}
+
+        <div className=" min-h-[20vh] w-full mt-2 rounded-l p-2">
+          <div className="container mx-auto p-4 bg-white">
+            <div className="flex justify-between mb-4">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+
+              <div className="flex space-x-4">
+                <button
+                  onClick={() => setopenTheSearch(!openTheSearch)}
+                  className="border border-gray-300  text-gray  py-1 px-5 rounded-md flex items-center gap-4 flex-row-reverse"
+                >
+                  Filter
+                  <IoFilterOutline />
+                </button>
+                <button className="btn btn-outline border-gray-300  flex items-center gap-4 flex-row-reverse">
+                  Export
+                  <RiDownloadLine />
+                </button>
+              </div>
+            </div>
+            <form
+              className={
+                openTheSearch ? "bg-[#F4F7FC] p-4 rounded-lg" : "hidden"
+              }
+              onSubmit={handleSubmit}
+            >
+              <div className="grid grid-cols-4 gap-4 ">
+                <div className="">
+                  <label
+                    htmlFor="id"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    ID
+                  </label>
+                  <input
+                    type="text"
+                    id="id"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="fullName"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="fullName"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="franchiseName"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Franchise Name
+                  </label>
+                  <input
+                    type="text"
+                    id="franchiseName"
+                    value={franchiseName}
+                    onChange={(e) => setFranchiseName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4 mt-4">
+                <div>
+                  <label
+                    htmlFor="plan"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Plan
+                  </label>
+                  <select
+                    id="plan"
+                    value={plan}
+                    onChange={(e) => setPlan(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  >
+                    <option value="All">All</option>
+                    <option value="Plan A">Plan A</option>
+                    <option value="Plan B">Plan B</option>
+                  </select>
+                </div>
+                <div>
+                  <label
+                    htmlFor="status"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Status
+                  </label>
+                  <select
+                    id="status"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  >
+                    <option value="All">All</option>
+                    <option value="Active">Active</option>
+                    <option value="Inactive">Inactive</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex justify-end mt-6">
+                <button
+                  type="button"
+                  className="  text-blue-300  py-2 px-4 rounded-md mr-4"
+                >
+                  Clear all
+                </button>
+                <button
+                  type="submit"
+                  className="bg-[#326DCF] hover:bg-blue-700 text-white  py-2 px-4 rounded-md"
+                >
+                  Show Results
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <div className="grid grid-cols-3 gap-4 my-3">
-          <div className="bg-white border shadow p-3 rounded-lg">
+          <div className="bg-white border shadow p-4 rounded-lg">
             <Typography variant="h6" className="font-bold mb-2 text-gray-500">
               Total Franchisor
             </Typography>
@@ -134,7 +389,7 @@ function ManageUser() {
               {rows.length}
             </Typography>
           </div>
-          <div className="bg-white border shadow p-5 rounded-lg">
+          <div className="bg-white border shadow p-4 rounded-lg">
             <Typography variant="h6" className="font-bold mb-2 text-gray-500">
               Pro Plan
             </Typography>
@@ -142,7 +397,7 @@ function ManageUser() {
               {rows.filter((row) => row.plan === "Pro").length}
             </Typography>
           </div>
-          <div className="bg-white border shadow p-3 rounded-lg">
+          <div className="bg-white border shadow p-4 rounded-lg">
             <Typography variant="h6" className="font-bold mb-2 text-gray-500">
               Basic Plan
             </Typography>
@@ -151,6 +406,7 @@ function ManageUser() {
             </Typography>
           </div>
         </div>
+
         <DataGrid
           rows={rows}
           columns={columns}
@@ -158,8 +414,14 @@ function ManageUser() {
           rowsPerPageOptions={[5, 10, 20]}
           checkboxSelection
           onSelectionModelChange={handleRowSelection}
+          sx={{
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: "#FAFBFA",
+            },
+          }}
         />
       </div>
+
       <Dialog open={openDialog} onClose={handleCloseDialog} fullWidth>
         <DialogTitle>Add New Franchisor</DialogTitle>
         <DialogContent>
